fed570ec3c2c9b69b4e9db28803bd9f8cb8a7e19
Giuliano Piscitani <giuliano.piscitani1@marist.edu>
2023-03-25 14:53:15 -0400

    set up basic express api

---
+++ index.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const express = require('express');
+
+const app = express();
+
+app.get('/endpoint-1', (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        data: {
+            message: "Hello from endpoint1"
+        }
+
+    })
+})
+app.get('/endpoint-2', (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        data: {
+            message: "Hello from endpoint1"
+        }
+
+    })
+})
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log('Server is running on this port ${port}'));
