cac47646fa9b3f397311f9548d55c9cbbbe21359
Giuliano Piscitani <giuliano.piscitani1@marist.edu>
2023-03-25 18:34:07 -0400

    useffect fetching backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/endpoint-1").then(
+       response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [] )
+  
   return (
     <div className="App">
       <header className="App-header">
